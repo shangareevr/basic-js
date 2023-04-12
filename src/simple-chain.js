94c0c680e92b04bfd2762e74ebf3d7c223388016
SHangareev Ruslan <sh.rusell@gmail.com>
2023-04-12 12:37:14 +0500

    feat: simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,23 +5,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain : [],
   getLength() {
+    return this.chain.length
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
+  addLink(value) {
+    this.chain.push(`( ${value} )`)
+    return this
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
-  removeLink(/* position */) {
+  removeLink(position) {
+    if ( !this.chain[position-1]) {
+      this.chain = []
+      throw new Error ("You can't remove incorrect link!")
+    }
+      this.chain.splice(position-1, 1);
+      return this
+    
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   reverseChain() {
+    this.chain.reverse()
+    return this
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   finishChain() {
+    const res = this.chain.join("~~");
+    this.chain = [];
+    return res
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
