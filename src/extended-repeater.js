72bcbb81c0d7b94f14f8b5d6cafc1f2534ba2338
SHangareev Ruslan <sh.rusell@gmail.com>
2023-04-12 12:18:56 +0500

    feat: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options*/) {
+function repeater(str, options) {
+  if(options.repeatTimes) {
+    options.repeatTimes = options.repeatTimes;
+  } else {
+    options.repeatTimes = 1;
+  }
+  if(options.separator) {
+    options.separator = String(options.separator);
+  } else {
+    options.separator = "+";
+  }
+  if(options.addition !==undefined) {
+    options.addition = String(options.addition);
+  } else {
+    options.addition = "";
+  }
+  if(options.additionRepeatTimes) {
+    options.additionRepeatTimes = options.additionRepeatTimes;
+  } else {
+    options.additionRepeatTimes = 1;
+  }
+  if(options.additionSeparator) {
+    options.additionSeparator = String(options.additionSeparator);
+  } else {
+    options.additionSeparator = "|";
+  }
+  let addStr = (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes);
+      addStr = addStr.slice(0, addStr.length - options.additionSeparator.length);
+  let res = (str+addStr+options.separator).repeat(options.repeatTimes);
+  return res.slice(0, res.length - options.separator.length)
   
 
   throw new NotImplementedError('Not implemented');
